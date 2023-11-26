65e281bb6dfed5b175437222a97237caeef18c82
noregret <noregret@noregret-hp.lan>
2023-11-26 20:52:39 +0800

    CPP

--- CPP/4.8.3.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    float x;
-    scanf("%f",&x);
-    printf("a.The input is %.1f or %.1e\n",x,x);
-    printf("b.The input is %+.3f or %+.3e\n",x,x);
-    return 0;
-}
